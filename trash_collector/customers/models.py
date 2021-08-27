c81c9d1158faf20b9d0c0a3aa9d0c5ad51415f4d
Kory Attleson <kory.attleson@gmail.com>
2021-08-27 13:23:43 -0500

    Created propterty objects for Customer class and Employee class

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import BooleanField, IntegerField
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -7,3 +8,12 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    balance = models.IntegerField()
+    weekly_pickup_day = models.CharField(max_length=50)
+    one_time_pickup = models.DateField()
+    suspend_start = models.DateField()
+    suspend_end = models.DateField()
+    suspend_status = BooleanField()
+    pickup_status = BooleanField()
